17a4167067612c768b1a65fc83c212f303429ffa
Borja <pintocorreas@gmail.com>
2023-09-25 10:13:51 +0200

    Funcion FizzBuzz

--- fizzBuzz/js/main.js
+++ fizzBuzz/js/main.js
@@ -1,11 +1,18 @@
-for(var i = 1; i <= 100; i++){
-    if(i % 3 === 0 && i % 5 ===0){
-        console.log("FizzBuzz");
-    }else if(i % 5 === 0){
-        console.log("Buzz");
-    }else if(i % 3 === 0){
-        console.log("Fizz");
-    }else{
-        console.log(i);
+function fizzBuzz(num1){
+    num1 = parseInt(prompt("Dime un numero"))
+    if(isNaN(num1) === true){
+        console.log("Escribe un numero")
     }
-}
+    for(var i = 1; i <= num1; i++){
+        if(i % 3 === 0 && i % 5 ===0){
+            console.log("FizzBuzz");
+        }else if(i % 5 === 0){
+            console.log("Buzz");
+        }else if(i % 3 === 0){
+            console.log("Fizz");
+        }else{
+            console.log(i);
+        }
+    }}
+
+fizzBuzz()
